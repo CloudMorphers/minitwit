20b92552f95c2b0064832cdecc7168f2db1c3289
Philip Mørch <mrphil2105@gmail.com>
2025-01-31 15:00:01 +0100

    Fix Python errors

--- minitwit.py
+++ minitwit.py
@@ -26,7 +26,7 @@ from flask import (
     g,
     flash,
 )
-from werkzeug import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 # configuration
@@ -47,7 +47,7 @@ def connect_db():
 def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
-        with app.open_resource("schema.sql") as f:
+        with app.open_resource("schema.sql", "r") as f:
             db.cursor().executescript(f.read())
         db.commit()
 
