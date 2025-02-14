a16c67929b0b29973ea17e4b353e9069479805bc
Philip Mørch <mrphil2105@gmail.com>
2025-02-14 14:56:58 +0100

    Create AppUser class

--- MiniTwit/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ MiniTwit/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MiniTwit.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(AppDbContext))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
