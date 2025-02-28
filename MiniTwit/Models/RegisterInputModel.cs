4096177a39f3e5ac6774f7a9e106d4072ffcaadc
Lucian <la@checkmate.dk>
2025-02-28 12:23:28 +0100

    Changed Password to pwd

--- MiniTwit/Models/RegisterInputModel.cs
+++ MiniTwit/Models/RegisterInputModel.cs
@@ -1,9 +1,6 @@
-namespace MiniTwit.Models
+public class RegisterInputModel
 {
-    public class RegisterInputModel
-    {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Pwd { get; set; }
 }
