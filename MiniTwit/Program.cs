f346a0704b929c52ee7ccb5e87427ad20fc43418
AlexLerche <98177274+AlexLerche@users.noreply.github.com>
2025-03-07 15:53:03 +0100

    Vagrant file (#17)
    * Change Password to Pwd
    
    * almost working
    
    * Vagrant file with docker dependencies

--- MiniTwit/Program.cs
+++ MiniTwit/Program.cs
@@ -1,8 +1,25 @@
-using Microsoft.AspNetCore.Identity;
+using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using MiniTwit.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Read the IP address from db_ip.txt and update the DefaultConnection string
+var ipFilePath = Path.Combine(AppContext.BaseDirectory, "db_ip.txt");
+if (File.Exists(ipFilePath))
+{
+    var ipAddress = File.ReadAllText(ipFilePath).Trim();
+    var dbConnectionString = $"DataSource={ipAddress};Cache=Shared";
+    builder.Configuration.AddInMemoryCollection(new[]
+    {
+        new KeyValuePair<string, string?>("ConnectionStrings:DefaultConnection", dbConnectionString)
+    });
+}
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -15,6 +32,7 @@ builder.Services.AddIdentity<AppUser, IdentityRole<int>>()
     .AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages(); // Add this line to add Razor Pages services
 
 builder.Services.Configure<IdentityOptions>(options =>
 {
@@ -32,25 +50,29 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+// Set the default port to 5040 when deploying
+app.Urls.Add("http://localhost:5040");
 
 app.Run();
