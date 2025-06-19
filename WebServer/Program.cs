96ef65da08764c7286e80719081a25ac0346c4a2
ceme <ceme@itu.dk>
2025-06-19 19:51:57 +0200

    Update Program.cs to match develop

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -4,78 +4,72 @@ using Microsoft.EntityFrameworkCore;
 using MiniTwit.Data;
 using Prometheus;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = Environment.GetEnvironmentVariable("MINITWIT_DB_CONNECTION_STRING");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentity<AppUser, IdentityRole<int>>().AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<IdentityOptions>(options =>
 {
-    public static void Main(string[] args)
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 1;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
     {
-        var builder = WebApplication.CreateBuilder(args);
+        var errorMessage = context
+            .ModelState.Values.SelectMany(value => value.Errors)
+            .Select(error => error.ErrorMessage)
+            .FirstOrDefault();
+        return new BadRequestObjectResult(new { status = 400, error_msg = errorMessage });
+    };
+});
 
-        var connectionString = Environment.GetEnvironmentVariable("MINITWIT_DB_CONNECTION_STRING");
-        builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+var app = builder.Build();
 
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-        builder.Services.AddIdentity<AppUser, IdentityRole<int>>().AddEntityFrameworkStores<AppDbContext>();
-
-        builder.Services.AddControllersWithViews();
-
-        builder.Services.Configure<IdentityOptions>(options =>
-        {
-            options.Password.RequireDigit = false;
-            options.Password.RequireLowercase = false;
-            options.Password.RequireNonAlphanumeric = false;
-            options.Password.RequireUppercase = false;
-            options.Password.RequiredLength = 1;
-            options.Password.RequiredUniqueChars = 1;
-        });
-
-        builder.Services.Configure<ApiBehaviorOptions>(options =>
-        {
-            options.InvalidModelStateResponseFactory = context =>
-            {
-                var errorMessage = context
-                    .ModelState.Values.SelectMany(value => value.Errors)
-                    .Select(error => error.ErrorMessage)
-                    .FirstOrDefault();
-                return new BadRequestObjectResult(new { status = 400, error_msg = errorMessage });
-            };
-        });
-
-        var app = builder.Build();
-
-        using (var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
-        {
-            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            context.Database.Migrate();
-        }
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseMigrationsEndPoint();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseRouting();
-
-        app.UseHttpMetrics();
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapStaticAssets();
-
-        app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}").WithStaticAssets();
-
-        app.MapGet("/health", () => Results.Ok("Healthy"));
-
-        app.UseMetricServer();
-
-        app.Run();
-    }
+using (var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
 }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseHttpMetrics();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}").WithStaticAssets();
+
+app.MapGet("/health", () => Results.Ok("Healthy"));
+
+app.UseMetricServer();
+
+app.Run();
